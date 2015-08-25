517672ec520e8be49299c1bd3ab652b94e81cfd5
msj <1019068644@qq.com>
2015-08-25 21:04:23 +0800

    string int

--- src/common/db.c
+++ src/common/db.c
@@ -136,7 +136,7 @@ int db_doWithOBJ(void (*func1)(const char*, int), void (*func2)(const char *))
     result = mysql_use_result(conn);
     while(row= mysql_fetch_row(result))
     {
-        func1(row[0], row[1]);
+        func1(row[0], atoi(row[1]));
         func2(row[0]);
     }
     mysql_free_result(result);
