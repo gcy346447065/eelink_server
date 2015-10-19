23ea87d99902f5157403b643711a7d9bb342499c
msj <1019068644@qq.com>
2015-10-19 16:35:14 +0800

    cgi 2 gps

---
+++ src/common/cgi2gps.c
@@ -0,0 +1,75 @@
+/*
+ * cgi2gps.c
+ *
+ *  Created on: Oct 19, 2015
+ *      Author: msj
+ */
+#include <curl/curl.h>
+#include <stdio.h>
+
+#include "cJSON.h"
+#include "macro.h"
+#include "log.h"
+
+#define MAX_LEN 400
+#define ADD2RXL 110
+
+typedef struct
+{
+   float lat;
+   float lon;
+}GPS;
+
+static void make_url(CGI_MC cell[], int cellNo, char *url)
+{
+    char *current = url;
+    int step = snprintf(current, MAX_LEN, "http://minigps.net/as?x=%x-%x", cell[0].mcc, cell[0].mnc);
+    int i;
+    for(i = 0, current += step; i < cellNo; ++i,current += step)
+    {
+        step = snprintf(current, MAX_LEN, "-%x-%x-%x", cell[i].lac, cell[i].ci, cell[i].rxl + ADD2RXL);
+    }
+    //TODO:&p&mt&ta&needaddress
+    snprintf(current, MAX_LEN, "&ta=1&needaddress=0");
+    return;
+}
+
+static size_t handleGetGps(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    GPS *gps = (GPS *)userp;
+
+    cJSON *gpsMsg = cJSON_Parse((const char *)buffer);
+    if(!gpsMsg)
+    {
+        LOG_ERROR("gps message format not json: %s", cJSON_GetErrorPtr());
+        return 1;
+    }
+    gps->lat = (cJSON_GetObjectItem(gpsMsg, "lat"))->valuedouble;
+    gps->lon = (cJSON_GetObjectItem(gpsMsg, "lon"))->valuedouble;
+    return 0;
+}
+
+int cgi2gps(CGI_MC cell[], int cellNo, float *lat, float *lon)
+{
+    GPS gps;
+    char url[MAX_LEN];
+    make_url(cell, cellNo, url);
+    CURL *curl = curl_easy_init();
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handleGetGps);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&gps);
+    
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    if(res != CURLE_OK)
+    {
+        return -1;
+    }
+    else
+    {
+        *lat = gps.lat;
+        *lon = gps.lon;
+        return 0;
+    }
+}
---
+++ src/common/cgi2gps.h
@@ -0,0 +1,9 @@
+/*
+ * cgi2gps.h
+ *
+ *  Created on: Oct 19, 2015
+ *      Author: msj
+ */
+#include "macro.h"
+
+ int cgi2gps(CGI_MC cell[], int cellNo, float *lat, float *lon);
