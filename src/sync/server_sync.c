26670fb3c5987cf522b5226a0223e1540f1aebc8
jiangtao <jiangtao@xiaoantech.com>
2015-11-30 09:14:17 +0800

    remove the timeout detection of sync server

--- src/sync/server_sync.c
+++ src/sync/server_sync.c
@@ -107,11 +107,6 @@ static void accept_conn_cb(struct evconnlistener *listener,
 	bufferevent_setcb(bev, read_cb, write_cb, event_cb, NULL);
 
 	bufferevent_enable(bev, EV_READ|EV_WRITE);
-
-	//set the timeout for the connection, when timeout close the connectiont
-	struct timeval tm = {300, 0};
-
-	bufferevent_set_timeouts(bev, &tm, &tm);
 }
 
 static void accept_error_cb(struct evconnlistener *listener, void *ctx)
