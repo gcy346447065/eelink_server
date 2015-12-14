ae0375eb5ae53b21d44fcd8b91628fe5fa3b0804
CentOS-xiaoanbao <guochengying@localhost.localdomain>
2015-12-14 21:29:01 +0800

    add db ping for reconnect

--- src/common/db.c
+++ src/common/db.c
@@ -18,7 +18,12 @@ static MYSQL *conn = NULL;
 
 int _db_initial()
 {
+    char value = 1;
+
     conn = mysql_init(NULL);
+
+    mysql_option(conn, MYSQL_OPT_RECONNECT, (char *)&value);
+
     if(!mysql_real_connect(conn, DB_HOST, DB_USER, DB_PWD, DB_NAME, DB_PORT, NULL, 0))
     {
         LOG_ERROR("can't connect database: %s(%u, %s)", DB_NAME, mysql_errno(conn), mysql_error(conn));
@@ -41,12 +46,19 @@ int _db_isTableCreated(const char* imeiName, int *num)
 {
     MYSQL_RES *res;
 
-    char reg[IMEI_LENGTH *2 + 5] = "gps_";
-    strncat(reg, imeiName, IMEI_LENGTH * 2);
+    char reg[IMEI_LENGTH + 5] = "gps_";
+    strncat(reg, imeiName, IMEI_LENGTH + 1);
+
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if((res = mysql_list_tables(conn, reg)) == NULL)
     {
         LOG_ERROR("can't judge whether tables of IMEI:%s(%u, %s)", imeiName, mysql_errno(conn), mysql_error(conn));
-        return 1;
+        return 2;
     }
 
     *num = mysql_num_rows(res);
@@ -61,12 +73,20 @@ int _db_createGPS(const char* tableName)
     char query[MAX_QUERY];
     //create table gps_IMEI(timestamp INT, lat DOUBLE, lon DOUBLE, speed TINYINT, course SMALLINT)
     snprintf(query, MAX_QUERY, "create table gps_%s(timestamp INT,lat DOUBLE(8,5),lon DOUBLE(8,5),speed TINYINT UNSIGNED,course SMALLINT,primary key(timestamp))", tableName);
+    
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_ERROR("can't create table: gps_%s(%u, %s)", tableName, mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     LOG_INFO("create table: gps_%s", tableName);
+
     return 0;
 }
 
@@ -75,12 +95,20 @@ int _db_createCGI(const char* tableName)
     char query[MAX_QUERY];
     //create table cgi_IMEI(timestamp INT, mcc SMALLINT, mnc SMALLINT, lac0 SMALLINT, ci0 SMALLINT, rxl0 SMALLINT...)
     snprintf(query, MAX_QUERY, "create table cgi_%s(timestamp INT,mcc SMALLINT,mnc SMALLINT,lac0 SMALLINT,ci0 SMALLINT,rxl0 SMALLINT,lac1 SMALLINT,ci1 SMALLINT,rxl1 SMALLINT,lac2 SMALLINT,ci2 SMALLINT,rxl2 SMALLINT,lac3 SMALLINT,ci3 SMALLINT,rxl3 SMALLINT,lac4 SMALLINT,ci4 SMALLINT,rxl4 SMALLINT,lac5 SMALLINT,ci5 SMALLINT,rxl5 SMALLINT,lac6 SMALLINT,ci6 SMALLINT,rxl6 SMALLINT)", tableName);
+    
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_ERROR("can't create table: cgi_%s(%u, %s)", tableName, mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     LOG_INFO("create table: cgi_%s", tableName);
+
     return 0;
 }
 
@@ -89,10 +117,17 @@ int _db_saveGPS(const char *imeiName, int timestamp, float lat, float lon, char 
     //timestamp INT, lat DOUBLE, lon DOUBLE, speed TINYINT, course SMALLINT
     char query[MAX_QUERY];
     snprintf(query, MAX_QUERY, "insert into gps_%s(timestamp,lat,lon,speed,course) values(%d,%f,%f,%u,%d)",imeiName, timestamp, lat, lon, speed, course);
+    
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_ERROR("can't insert into gps_%s(%u, %s)", imeiName, mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     LOG_INFO("insert into gps_%s: %d, %f, %f, %u, %d", imeiName, timestamp, lat, lon, speed, course);
     return 0;
@@ -116,10 +151,17 @@ int _db_saveCGI(const char *imeiName, int timestamp, const CGI_MC cell[], int ce
         current += step - 1;
         step = snprintf(current, MAX_QUERY, ",%d,%d,%d)", (unsigned short)cell[i].lac, (unsigned short)cell[i].ci, cell[i].rxl);
     }
+
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_ERROR("can't insert into cgi_%s(%u, %s)", imeiName, mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     LOG_INFO(query);
     return 0;
@@ -130,10 +172,17 @@ Names of the table and columns need modifing*/
 int _db_doWithOBJ(void (*func1)(const char*, int), void (*func2)(const char *))
 {
     char query[] = "select imei, lastlogintime from object";
+
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_FATAL("can't get objects from db(%u, %s)", mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     MYSQL_RES *result;
     MYSQL_ROW row;
@@ -151,10 +200,17 @@ int _db_insertOBJ(const char *imeiName, int lastLoginTime)
 {
     char query[MAX_QUERY];
     snprintf(query, MAX_QUERY, "insert into object(imei, lastlogintime) values(\'%s\', %d)", imeiName, lastLoginTime);
+    
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_ERROR("can't insert %s into object(%u, %s)", imeiName, mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     return 0;
 }
@@ -163,10 +219,17 @@ int _db_updateOBJ(const char *imeiName, int lastLoginTime)
 {
     char query[MAX_QUERY];
     snprintf(query, MAX_QUERY, "update object set lastlogintime = %d where imei = \'%s\'", lastLoginTime, imeiName);
+    
+    if(mysql_ping(conn))
+    {
+        LOG_ERROR("can't ping mysql(%u, %s)",mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
     if(mysql_query(conn, query))
     {
         LOG_ERROR("can't update Obj where imei = %s(%u, %s)", imeiName, mysql_errno(conn), mysql_error(conn));
-        return -1;
+        return 2;
     }
     return 0;
 }
