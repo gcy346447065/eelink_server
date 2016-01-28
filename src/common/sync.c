4701aa709b5edbe3524f98313ee596796b066998
Jason <Jerson.G@gmail.com>
2016-01-28 19:59:20 +0800

    remove unused code

--- src/common/sync.c
+++ src/common/sync.c
@@ -107,7 +107,6 @@ int sync_init(struct event_base *base)
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = htonl(0x7f000001); /* 127.0.0.1 */
     sin.sin_port = htons(PORT_SYNC);
-    //sin.sin_port = htons(8889);
 
     bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
 
