1582dea31ae73753fda2c9ecafc55f2c92d466a9
jiangtao <jiangtao@xiaoantech>
2016-06-25 16:49:38 +0800

    add message structure

---
+++ src/gps/Message.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jk on 16-6-25.
+//
+
+#include "Message.h"
+
+void Message::process() {
+
+}
---
+++ src/gps/Message.h
@@ -0,0 +1,27 @@
+//
+// Created by jk on 16-6-25.
+//
+
+#ifndef ELECTROMBILE_MESSAGE_H
+#define ELECTROMBILE_MESSAGE_H
+
+
+class Message {
+public:
+    static const int IMEI_LENGTH = 15;
+
+private:
+    short signature;
+    char imei[IMEI_LENGTH];
+    char cmd;
+    short length;
+    char data[0];
+
+
+public:
+    void process();
+
+};
+
+
+#endif //ELECTROMBILE_MESSAGE_H
