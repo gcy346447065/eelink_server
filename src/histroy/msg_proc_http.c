f0b13f03a395c3b06fefdc238f245bd5a80af71d
piggysticker <piggysticker>
2016-10-19 16:48:34 +0800

    修改bug

--- src/histroy/msg_proc_http.c
+++ src/histroy/msg_proc_http.c
@@ -16,24 +16,22 @@
 
 char *history_getGPS(const char *imeiName, int starttime, int endtime)
 {
-    cJSON *rsp = NULL;
+    cJSON *gps_Array = NULL;
+    cJSON *rsp = cJSON_CreateObject();;
     HISTORY_GPS_RSP *gps = (HISTORY_GPS_RSP *)db_getGPS(imeiName, starttime, endtime);
 
     if(!gps)
     {
-        rsp = cJSON_CreateObject();
         cJSON_AddNumberToObject(rsp, "code", 101);
     }
     else if(gps->num == 0)
     {
-        LOG_INFO("get gps number:%d",gps->num);
-        rsp = cJSON_CreateObject();
         cJSON_AddNumberToObject(rsp, "code", 102);
     }
     else
     {
-        rsp = cJSON_CreateArray();
         cJSON *iGps = NULL;
+        gps_Array = cJSON_CreateArray();
         LOG_INFO("get gps number:%d",gps->num);
         for(int i = 0; i < gps->num;i++)
         {
@@ -43,8 +41,9 @@ char *history_getGPS(const char *imeiName, int starttime, int endtime)
             cJSON_AddNumberToObject(iGps, "lon", gps->gps[i].longitude);
             cJSON_AddNumberToObject(iGps, "speed", gps->gps[i].speed);
             cJSON_AddNumberToObject(iGps, "course", gps->gps[i].course);
-            cJSON_AddItemToArray(rsp, iGps);
+            cJSON_AddItemToArray(gps_Array, iGps);
         }
+        cJSON_AddItemToObject(rsp, "gps", gps_Array);
     }
     char *json = cJSON_PrintUnformatted(rsp);
     LOG_INFO("%s",json);
