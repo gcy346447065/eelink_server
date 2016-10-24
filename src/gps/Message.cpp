7f5371838ad3a70133fbb13efc8070d6fdb442fd
Jason <Jerson.G@gmail.com>
2016-10-24 15:21:48 +0800

    简化代码

--- src/gps/Message.cpp
+++ src/gps/Message.cpp
@@ -13,13 +13,11 @@ using namespace std;
 void Message::process()
 {
     GPS *gps = reinterpret_cast<GPS*> (data);
-    GPS *pGPS = NULL;
 
     string IMEI(imei, imei + IMEI_LENGTH);
 
     for(int i = 0; i < ntohs(length)/sizeof(GPS); i++)
     {
-        pGPS = (GPS *)gps + i;
-        DB::instance().addGPS(IMEI, pGPS);
+        DB::instance().addGPS(IMEI, gps + i);
     }
 }
