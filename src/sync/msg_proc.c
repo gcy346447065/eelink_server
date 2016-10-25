f1a9f2c93d24197e130eea53e475312b99369b9a
Jason <Jerson.G@gmail.com>
2016-10-25 22:13:33 +0800

    暂时停止里程数据和sim卡信息上报leancloud

--- src/sync/msg_proc.c
+++ src/sync/msg_proc.c
@@ -161,11 +161,11 @@ int handle_incoming_msg(const char *m, size_t msgLen, void *arg)
                 break;
 
             case CMD_SYNC_NEW_ITINERARY:
-                msg_saveItinerary(root);
+//                msg_saveItinerary(root);
                 break;
 
             case CMD_SYNC_NEW_SIM_INFO:
-                msg_saveSimInfo(root);
+//                msg_saveSimInfo(root);
                 break;
 
             default:
