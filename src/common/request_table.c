bb68a60309779a2effc4ea656d12ebd718c22d5a
DESKTOP-V9AVBLC\piggysticker <641777437@qq.com>
2016-12-08 14:52:40 +0800

    before destory, return the rest reply

--- src/common/request_table.c
+++ src/common/request_table.c
@@ -11,15 +11,10 @@
 #include "msg_http.h"
 #include "log.h"
 
-//before request_del req has been retruned to http, so no need to free it
+//before request_del req has been used to return to http, so no need to free it
 GHashTable *request_initial(void)
 {
     return g_hash_table_new_full(g_direct_hash, g_direct_equal, NULL, NULL);
-}
-
-void request_destruct(GHashTable *request_table)
-{
-    g_hash_table_destroy(request_table);
 }
 
 int request_add(GHashTable *request_table, struct evhttp_request *req, unsigned char seq)
@@ -37,5 +32,22 @@ int request_del(GHashTable *request_table, unsigned char seq)
 struct evhttp_request *request_get(GHashTable *request_table, unsigned char seq)
 {
     return g_hash_table_lookup(request_table, (gconstpointer)seq);
+}
+
+static void request_sendReply(gpointer key __attribute__((unused)), gpointer value, gpointer user_data __attribute__((unused)))
+{
+    http_errorReply((struct evhttp_request *)value, CODE_DEVICE_OFF);
+}
+
+static void request_replyRest(GHashTable *request_table)
+{
+    g_hash_table_foreach(request_table, (GHFunc)request_sendReply, NULL);
+}
+
+//when session disconnect ,reply the rest and destroy the table
+void request_destruct(GHashTable *request_table)
+{
+    request_replyRest(request_table);
+    g_hash_table_destroy(request_table);
 }
 
