c4ce26aa81c0c8f5962890f9b307fda02c07e4d1
DESKTOP-V9AVBLC\piggysticker <641777437@qq.com>
2016-12-26 11:01:16 +0800

    去掉 syn GPS和 itinerary同步

--- src/sync/msg_proc.c
+++ src/sync/msg_proc.c
@@ -171,11 +171,11 @@ int handle_incoming_msg(const char *m, size_t msgLen, void *arg)
                 break;
 
             case CMD_SYNC_NEW_GPS:
-                msg_saveGPS(root);
+//                msg_saveGPS(root);
                 break;
 
             case CMD_SYNC_NEW_ITINERARY:
-                msg_saveItinerary(root);
+//                msg_saveItinerary(root);
                 break;
 
             case CMD_SYNC_NEW_SIM_INFO:
