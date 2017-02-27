b68a720deaf6607adff575f5cfb7bc2d3071bd17
DESKTOP-V9AVBLC\piggysticker <641777437@qq.com>
2017-02-27 14:16:28 +0800

    mqtt接口统一到 port.h

--- src/common/port.h
+++ src/common/port.h
@@ -6,6 +6,8 @@
 #define ELECTROMBILE_PORT_H
 
 #define HOST_SIMCOM     "test.xiaoan110.com"
+#define HOST_MQTT       "innerredis.xiaoan110.com"
+
 #define HOST_REDIS      "innerredis.xiaoan110.com"
 #define AUTH_REDIS      "Bbj4VzYmDTufJU8KPBP9FKj5"
 
@@ -16,5 +18,6 @@
 #define PORT_HTTP       8081
 #define PORT_SIMCOMHTTP 8082
 #define PORT_REDIS      6379
+#define PORT_MQTT       1883
 
 #endif //ELECTROMBILE_PORT_H
